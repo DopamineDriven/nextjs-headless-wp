c0a34701d2aad65a8a7515ed1d3c90a839118c89
Andrew Ross <andrew.ross@cortinahealth.com>
2022-03-24 05:03:25 -0500

    article published

--- src/components/Icons/x-icon.tsx
+++ src/components/Icons/x-icon.tsx
@@ -1,19 +1,16 @@
-import { SVGAttribs } from '@/types/mapped';
+import { VFC } from "react";
 
-const X = ({
-  ...props
-}: SVGAttribs<
-  'className' | 'width' | 'height' | 'onMouseOver' | 'aria-hidden' | "onClick"
->) => {
+const X: VFC<ReactUnwrapped<"svg" | "path">> = ({ svg, path }) => {
   return (
     <svg
-      width='24'
-      height='24'
+      width={svg?.width ? svg.width : 24}
+      height={svg?.height ? svg.height : 24}
       viewBox='0 0 24 24'
-      fill='none'
+      fill={svg?.fill ? svg.fill : "none"}
       xmlns='http://www.w3.org/2000/svg'
-      {...props}>
+      {...svg}>
       <path
+        {...path}
         d='M6 6L18 18M6 18L18 6L6 18Z'
         stroke='rgb(29, 78, 216)'
         strokeWidth='2'
