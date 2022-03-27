2892b2ca8e24a7729c7adf99cae09bdd6af931da
Andrew Ross <andrew.ross@cortinahealth.com>
2022-03-27 06:01:36 -0500

    repo directory deepening incoming to house turborepo

--- unwrap.d.ts
+++ unwrap.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="react" />
+
 export declare module Unwrap {
   /**
    * @type ExciseEnumerble<T> = T extends Array<infer U> ? U : T;
@@ -81,6 +83,240 @@ export declare module Unwrap {
       ? Record<keyof U, U>
       : Record<keyof T, T>
   > = T;
+
+  type HTMLElementUnion =
+    | HTMLAnchorElement
+    | HTMLAreaElement
+    | HTMLAudioElement
+    | HTMLBRElement
+    | HTMLBaseElement
+    | HTMLBodyElement
+    | HTMLButtonElement
+    | HTMLCanvasElement
+    | HTMLDListElement
+    | HTMLDataElement
+    | HTMLDataListElement
+    | HTMLDetailsElement
+    | HTMLDivElement
+    | HTMLElement
+    | HTMLEmbedElement
+    | HTMLFieldSetElement
+    | HTMLFormElement
+    | HTMLHRElement
+    | HTMLHeadElement
+    | HTMLHeadingElement
+    | HTMLHtmlElement
+    | HTMLIFrameElement
+    | HTMLImageElement
+    | HTMLInputElement
+    | HTMLLIElement
+    | HTMLLabelElement
+    | HTMLLegendElement
+    | HTMLLinkElement
+    | HTMLMapElement
+    | HTMLMediaElement
+    | HTMLMenuElement
+    | HTMLMetaElement
+    | HTMLMeterElement
+    | HTMLModElement
+    | HTMLOListElement
+    | HTMLObjectElement
+    | HTMLOptGroupElement
+    | HTMLOptionElement
+    | HTMLParagraphElement
+    | HTMLParamElement
+    | HTMLPictureElement
+    | HTMLProgressElement
+    | HTMLQuoteElement
+    | HTMLScriptElement
+    | HTMLSelectElement
+    | HTMLSlotElement
+    | HTMLSourceElement
+    | HTMLSpanElement
+    | HTMLStyleElement
+    | HTMLTableCaptionElement
+    | HTMLTableCellElement
+    | HTMLTableColElement
+    | HTMLTableElement
+    | HTMLTableRowElement
+    | HTMLTableSectionElement
+    | HTMLTemplateElement
+    | HTMLTextAreaElement
+    | HTMLTimeElement
+    | HTMLTitleElement
+    | HTMLTrackElement
+    | HTMLUListElement
+    | HTMLUnknownElement
+    | HTMLVideoElement
+    | HTMLWebViewElement;
+
+  type HTMLAttributesUnion<T> =
+    | HTMLAttributes<T>
+    | AllHTMLAttributes<T>
+    | AnchorHTMLAttributes<T>
+    | AreaHTMLAttributes<T>
+    | AudioHTMLAttributes<T>
+    | BaseHTMLAttributes<T>
+    | BlockquoteHTMLAttributes<T>
+    | ButtonHTMLAttributes<T>
+    | CanvasHTMLAttributes<T>
+    | ColHTMLAttributes<T>
+    | ColgroupHTMLAttributes<T>
+    | DataHTMLAttributes<T>
+    | DetailsHTMLAttributes<T>
+    | DelHTMLAttributes<T>
+    | DialogHTMLAttributes<T>
+    | EmbedHTMLAttributes<T>
+    | FieldsetHTMLAttributes<T>
+    | FormHTMLAttributes<T>
+    | HtmlHTMLAttributes<T>
+    | IframeHTMLAttributes<T>
+    | ImgHTMLAttributes<T>
+    | InputHTMLAttributes<T>
+    | InsHTMLAttributes<T>
+    | KeygenHTMLAttributes<T>
+    | LabelHTMLAttributes<T>
+    | LiHTMLAttributes<T>
+    | LinkHTMLAttributes<T>
+    | MapHTMLAttributes<T>
+    | MenuHTMLAttributes<T>
+    | MediaHTMLAttributes<T>
+    | MetaHTMLAttributes<T>
+    | MeterHTMLAttributes<T>
+    | QuoteHTMLAttributes<T>
+    | ObjectHTMLAttributes<T>
+    | OlHTMLAttributes<T>
+    | OptgroupHTMLAttributes<T>
+    | OptionHTMLAttributes<T>
+    | OutputHTMLAttributes<T>
+    | ParamHTMLAttributes<T>
+    | ProgressHTMLAttributes<T>
+    | SlotHTMLAttributes<T>
+    | ScriptHTMLAttributes<T>
+    | SelectHTMLAttributes<T>
+    | SourceHTMLAttributes<T>
+    | StyleHTMLAttributes<T>
+    | TableHTMLAttributes<T>
+    | TextareaHTMLAttributes<T>
+    | TdHTMLAttributes<T>
+    | ThHTMLAttributes<T>
+    | TimeHTMLAttributes<T>
+    | TrackHTMLAttributes<T>
+    | VideoHTMLAttributes<T>
+    | WebViewHTMLAttributes<T>;
+
+  type UnwrapHtmlUnion<
+    H extends HTMLElementUnion,
+    T extends HTMLAttributesUnion<H>
+  > = {
+    [L in keyof DetailedHTMLProps<T, H>]?: DetailedHTMLProps<T, H>[L];
+  };
+
+  type SvgElementUnion =
+    | SVGAElement
+    | SVGAnimateElement
+    | SVGAngle
+    | SVGAnimateMotionElement
+    | SVGAnimateTransformElement
+    | SVGAnimatedAngle
+    | SVGAnimatedBoolean
+    | SVGAnimatedEnumeration
+    | SVGAnimatedInteger
+    | SVGAnimatedLength
+    | SVGAnimatedLengthList
+    | SVGAnimatedNumber
+    | SVGAnimatedNumberList
+    | SVGAnimatedPoints
+    | SVGAnimatedPreserveAspectRatio
+    | SVGAnimatedRect
+    | SVGAnimatedString
+    | SVGAnimatedTransformList
+    | SVGAnimationElement
+    | SVGBoundingBoxOptions
+    | SVGCircleElement
+    | SVGClipPathElement
+    | SVGComponentTransferFunctionElement
+    | SVGDefsElement
+    | SVGDescElement
+    | SVGElement
+    | SVGEllipseElement
+    | SVGFEBlendElement
+    | SVGFEColorMatrixElement
+    | SVGFEComponentTransferElement
+    | SVGFECompositeElement
+    | SVGFEConvolveMatrixElement
+    | SVGFEDiffuseLightingElement
+    | SVGFEDistantLightElement
+    | SVGFEDropShadowElement
+    | SVGFEFloodElement
+    | SVGFEFuncAElement
+    | SVGFEFuncBElement
+    | SVGFEFuncGElement
+    | SVGFEFuncRElement
+    | SVGFEGaussianBlurElement
+    | SVGFEImageElement
+    | SVGFEMergeElement
+    | SVGFEMergeNodeElement
+    | SVGFEMorphologyElement
+    | SVGFEOffsetElement
+    | SVGFEPointLightElement
+    | SVGFESpecularLightingElement
+    | SVGFESpotLightElement
+    | SVGFETileElement
+    | SVGFETurbulenceElement
+    | SVGFilterElement
+    | SVGFitToViewBox
+    | SVGForeignObjectElement
+    | SVGGElement
+    | SVGGeometryElement
+    | SVGGradientElement
+    | SVGGraphicsElement
+    | SVGImageElement
+    | SVGLineElement
+    | SVGLinearGradientElement
+    | SVGMPathElement
+    | SVGMarkerElement
+    | SVGMaskElement
+    | SVGMetadataElement
+    | SVGPathElement
+    | SVGPatternElement
+    | SVGPolygonElement
+    | SVGPolylineElement
+    | SVGRadialGradientElement
+    | SVGRectElement
+    | SVGSVGElement
+    | SVGScriptElement
+    | SVGSetElement
+    | SVGStopElement
+    | SVGStyleElement
+    | SVGSwitchElement
+    | SVGSymbolElement
+    | SVGTSpanElement
+    | SVGTextContentElement
+    | SVGTextElement
+    | SVGTextPathElement
+    | SVGTextPositioningElement
+    | SVGTitleElement
+    | SVGUseElement
+    | SVGViewElement;
+
+  type UnwrapSvgUnion<
+    T extends SvgElementUnion,
+    P extends keyof SVGProps<T>
+  > = {
+    [R in P]?: SVGProps<T>[R];
+  };
+  //   export type PickFromHTMLUnion<T extends HTMLElementUnion> = DeepPartial<HTMLAttributesUnion<T>>
+
+  /* interface with all optional values to accept ONLY one of them */
+  type ConditionalSansOne<T, Keys extends keyof T = keyof T> = Pick<
+    T,
+    Exclude<keyof T, Keys>
+  > &
+    {
+      [K in Keys]-?: Required<Pick<T, K>> &
+        Partial<Record<Exclude<Keys, K>, undefined>>;
+    }[Keys];
 }
 // map out the children props of each recursed JSX.IntrinsicElement via
 // an additional round of conditional recursion for type deepening
